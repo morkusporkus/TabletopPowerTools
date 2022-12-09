06d60ba1420c11212545a23c3468c1b770f03a57
Marcus Mihulka <464531@corp.caseys.com>
2022-12-09 16:15:15 -0600

    cleanup

--- DMPowerTools/MauiProgram.cs
+++ DMPowerTools/MauiProgram.cs
@@ -1,37 +1,35 @@
-﻿using Microsoft.Extensions.Logging;
-using DMPowerTools.Data;
+﻿using DMPowerTools.Data;
+using Microsoft.Extensions.Logging;
 using MudBlazor.Services;
-using System.Linq.Expressions;
-using System;
 
 namespace DMPowerTools;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-		builder.Services.AddMauiBlazorWebView();
-		builder.Services.AddDbContext<ApplicationDbContext>();
+        builder.Services.AddMauiBlazorWebView();
+        builder.Services.AddDbContext<ApplicationDbContext>();
 
 #if DEBUG
         builder.Services.AddBlazorWebViewDeveloperTools();
-		builder.Logging.AddDebug();
+        builder.Logging.AddDebug();
 #endif
         builder.Services.AddMudServices();
         var app = builder.Build();
 
-		using var scope = app.Services.CreateScope();
+        using var scope = app.Services.CreateScope();
 
-		var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-		 dbContext.Database.EnsureCreated(); 
-		return app;
-	}
+        var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        dbContext.Database.EnsureCreated();
+        return app;
+    }
 }
