3d0227284d31930bb3599b46c485c81ae3372716
Marcus Mihulka <464531@corp.caseys.com>
2022-12-19 15:37:48 -0600

    Add Add if not exists

---
+++ DMPowerTools/Data/Extensions/DbSetExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMPowerTools.Data.Extensions
+{
+    public static class DbSetExtensions
+    {
+
+        public static EntityEntry<T> AddIfNotExists<T>(this DbSet<T> dbSet, T entity, Expression<Func<T, bool>> predicate = null) where T : class, new()
+        {
+            var exists = predicate != null ? dbSet.Any(predicate) : dbSet.Any();
+            return !exists ? dbSet.Add(entity) : null;
+        }
+    }
+}
