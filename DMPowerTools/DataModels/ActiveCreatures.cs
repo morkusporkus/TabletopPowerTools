0af6efd4ed6260c39ccd6d201e2b0c78567ed6c0
morkusporkus <bearsss111@gmail.com>
2022-12-28 12:32:28 -0600

    update

---
+++ DMPowerTools/DataModels/ActiveCreatures.cs
@@ -0,0 +1,18 @@
+﻿using DMPowerTools.Data;
+
+namespace DMPowerTools.DataModels
+{
+    public class ActiveCreature
+    {
+        public ActiveCreature(int id, int inititiveRoll, Creature creature)
+        {
+            Id = id;
+            InititiveRoll = inititiveRoll;
+            Creature = creature;
+        }
+
+        public int Id { get; set; }
+        public int InititiveRoll { get; set; }
+        public Creature Creature { get; set; }
+    }
+}
