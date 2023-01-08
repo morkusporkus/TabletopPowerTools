bf380e4b010cab99f18b47b018add2c925a868ed
Marcus Mihulka <464531@corp.caseys.com>
2023-01-08 09:28:57 -0600

    update

--- src/Maui/Features/Creatures/Shared/CreatureDetail/CreatureDetail.razor.cs
+++ src/Maui/Features/Creatures/Shared/CreatureDetail/CreatureDetail.razor.cs
@@ -1,5 +1,11 @@
-﻿namespace DMPowerTools.Maui.Features.Creatures.Shared.CreatureDetail;
+﻿using Markdig;
+
+namespace DMPowerTools.Maui.Features.Creatures.Shared.CreatureDetail;
 public partial class CreatureDetail
 {
     [Parameter, EditorRequired] public Creature Creature { get; set; }
+    public MarkupString MarkdownDisplay(string item)
+    {
+        return (MarkupString)Markdown.ToHtml(item);
+    }
 }
