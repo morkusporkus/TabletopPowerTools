62023446f8b3775fcb5b8667f01e9a157d8a7358
Ben Sampica <benjamin.sampica@gmail.com>
2023-01-08 23:48:03 -0600

    Icons for damage types + others

---
+++ src/Maui/Shared/RpgIcon.cs
@@ -0,0 +1,64 @@
+﻿namespace DMPowerTools.Maui.Shared;
+
+public class RpgIcon
+{
+    public class DamageTypes
+    {
+        public const string Acid = Icons.Material.TwoTone.Science;
+        public const string Bludgeoning = Icons.Material.TwoTone.Gavel;
+        public const string Cold = Icons.Material.TwoTone.AcUnit;
+        public const string Fire = Icons.Material.TwoTone.LocalFireDepartment;
+        public const string Force = Icons.Material.TwoTone.Deblur;
+        public const string Lightning = Icons.Material.TwoTone.Bolt;
+        public const string Necrotic = Icons.Custom.Uncategorized.BioHazard;
+        public const string Piercing = Icons.Material.TwoTone.ArrowOutward;
+        public const string Psychic = Icons.Material.TwoTone.WifiTethering;
+        public const string Radiant = Icons.Material.TwoTone.WbTwilight;
+        public const string Slashing = Icons.Material.TwoTone.Edit;
+        public const string Thunder = Icons.Material.TwoTone.Thunderstorm;
+    }
+
+    public class Creature
+    {
+        public const string ArmorClass = Icons.Material.TwoTone.Shield;
+        public const string Movement = Icons.Material.TwoTone.MoveUp;
+        public const string HitPoints = Icons.Material.TwoTone.Bloodtype;
+
+        public class Attributes
+        {
+            public const string Strength = "";
+            public const string Dexterity = "";
+            public const string Constitution = "";
+            public const string Wisdom = "";
+            public const string Intelligence = "";
+            public const string Charisma = "";
+        }
+
+        public class Skills
+        {
+            public const string Acrobatics = "";
+            public const string AnimalHandling = "";
+            public const string Arcana = "";
+            public const string Athletics = "";
+            public const string Deception = "";
+            public const string History = Icons.Material.TwoTone.Book;
+            public const string Insight = "";
+            public const string Intimidation = "";
+            public const string Investigation = Icons.Material.TwoTone.PersonSearch;
+            public const string Medicine = Icons.Material.TwoTone.HealthAndSafety;
+            public const string Nature = Icons.Material.TwoTone.Nature;
+            public const string Perception = Icons.Material.TwoTone.ImageSearch;
+            public const string Performance = "";
+            public const string Persuasion = "";
+            public const string Religion = "";
+            public const string SleightOfHand = Icons.Material.TwoTone.BackHand;
+            public const string Stealth = "";
+            public const string Survival = "";
+        }
+    }
+
+    public class Misc
+    {
+        public const string Dice = Icons.Material.TwoTone.Casino;
+    }
+}
