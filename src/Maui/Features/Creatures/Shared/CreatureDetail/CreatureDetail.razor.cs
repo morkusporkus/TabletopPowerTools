6a37b204d41054857e0333aeb6765ae58ba8b00d
scuriel <halsten.samantha@gmail.com>
2023-01-11 22:59:38 -0600

    add CreatureProperty

--- src/Maui/Features/Creatures/Shared/CreatureDetail/CreatureDetail.razor.cs
+++ src/Maui/Features/Creatures/Shared/CreatureDetail/CreatureDetail.razor.cs
@@ -1,6 +1,5 @@
 ﻿using DMPowerTools.Core.Features.Creatures.Shared;
 using DMPowerTools.Core.Models;
-using Markdig;
 
 namespace DMPowerTools.Maui.Features.Creatures.Shared.CreatureDetail;
 public partial class CreatureDetail
@@ -17,10 +16,5 @@ public partial class CreatureDetail
     {
         if (Creature is null) _response = await Mediator.Send(new CreatureDetailQuery { Id = CreatureId });
         else _response = new() { Creature = Creature };
-    }
-
-    public static MarkupString MarkdownDisplay(string item)
-    {
-        return (MarkupString)Markdown.ToHtml(item);
     }
 }
