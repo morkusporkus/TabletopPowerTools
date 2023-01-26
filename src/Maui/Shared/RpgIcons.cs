a223d694076a57a8444dabfee3e698881ba1e47c
Ben Sampica <benjamin.sampica@gmail.com>
2023-01-25 21:33:46 -0600

    Add damage icons.

--- src/Maui/Shared/RpgIcons.cs
+++ src/Maui/Shared/RpgIcons.cs
@@ -2,63 +2,70 @@
 
 public class RpgIcons
 {
+    private const string _baseClass = "game-icon game-icon-";
+
     public class DamageType
     {
-        public const string Acid = Icons.Material.TwoTone.Science;
-        public const string Bludgeoning = Icons.Material.TwoTone.Gavel;
-        public const string Cold = Icons.Material.TwoTone.AcUnit;
-        public const string Fire = Icons.Material.TwoTone.LocalFireDepartment;
-        public const string Force = Icons.Material.TwoTone.Deblur;
-        public const string Lightning = Icons.Material.TwoTone.Bolt;
-        public const string Necrotic = Icons.Custom.Uncategorized.BioHazard;
-        public const string Piercing = Icons.Material.TwoTone.ArrowOutward;
-        public const string Psychic = Icons.Material.TwoTone.WifiTethering;
-        public const string Radiant = Icons.Material.TwoTone.WbTwilight;
-        public const string Slashing = Icons.Material.TwoTone.Edit;
-        public const string Thunder = Icons.Material.TwoTone.Thunderstorm;
+        public const string Acid = _baseClass + "acid";
+        public const string Bludgeoning = _baseClass + "thor-hammer";
+        public const string Cold = _baseClass + "snowflake-1";
+        public const string Fire = _baseClass + "fire";
+        public const string Force = _baseClass + "ringed-beam";
+        public const string Lightning = _baseClass + "lightning-storm";
+        public const string Necrotic = _baseClass + "broken-skull";
+        public const string Piercing = _baseClass + "spear-hook";
+        public const string Psychic = _baseClass + "psychic-waves";
+        public const string Radiant = _baseClass + "sun-radiations";
+        public const string Slashing = _baseClass + "saber-slash";
+        public const string Thunder = _baseClass + "earth-spit";
     }
 
     public class Creature
     {
-        public const string ArmorClass = Icons.Material.TwoTone.Shield;
-        public const string Movement = Icons.Material.TwoTone.MoveUp;
-        public const string HitPoints = Icons.Material.TwoTone.Bloodtype;
+        public const string ArmorClass = _baseClass + "chest-armor";
+        public const string Movement = _baseClass + "walking-boot";
+        public const string HitPoints = _baseClass + "hearts";
 
         public class Attribute
         {
-            public const string Strength = "";
-            public const string Dexterity = "";
-            public const string Constitution = "";
-            public const string Wisdom = "";
-            public const string Intelligence = "";
-            public const string Charisma = "";
+            public const string Strength = _baseClass + "lion";
+            public const string Dexterity = _baseClass + "feline";
+            public const string Constitution = _baseClass + "bear-head";
+            public const string Wisdom = _baseClass + "owl";
+            public const string Intelligence = _baseClass + "fox-head";
+            public const string Charisma = _baseClass + "eagle-head";
         }
 
         public class Skill
         {
-            public const string Acrobatics = "";
-            public const string AnimalHandling = "";
-            public const string Arcana = "";
-            public const string Athletics = "";
-            public const string Deception = "";
-            public const string History = Icons.Material.TwoTone.Book;
-            public const string Insight = "";
-            public const string Intimidation = "";
-            public const string Investigation = Icons.Material.TwoTone.PersonSearch;
-            public const string Medicine = Icons.Material.TwoTone.HealthAndSafety;
-            public const string Nature = Icons.Material.TwoTone.Nature;
-            public const string Perception = Icons.Material.TwoTone.ImageSearch;
-            public const string Performance = "";
-            public const string Persuasion = "";
-            public const string Religion = "";
-            public const string SleightOfHand = Icons.Material.TwoTone.BackHand;
-            public const string Stealth = "";
-            public const string Survival = "";
+            public const string Acrobatics = _baseClass + "contortionist";
+            public const string AnimalHandling = _baseClass + "cavalry";
+            public const string Arcana = _baseClass + "spell-book";
+            public const string Athletics = _baseClass + "muscle-up";
+            public const string Deception = _baseClass + "convince";
+            public const string History = _baseClass + "backward-time";
+            public const string Insight = _baseClass + "inner-self";
+            public const string Intimidation = _baseClass + "one-eyed";
+            public const string Investigation = _baseClass + "magnifying-glass";
+            public const string Medicine = _baseClass + "apothecary";
+            public const string Nature = _baseClass + "forest";
+            public const string Perception = _baseClass + "semi-closed-eye";
+            public const string Performance = _baseClass + "sing";
+            public const string Persuasion = _baseClass + "public-speaker";
+            public const string Religion = _baseClass + "ankh";
+            public const string SleightOfHand = _baseClass + "snatch";
+            public const string Stealth = _baseClass + "cloak-dagger";
+            public const string Survival = _baseClass + "deer-track";
+        }
+
+        public class Condition
+        {
+            public const string Grappled = _baseClass + "grapple";
         }
     }
 
     public class Miscellaneous
     {
-        public const string Dice = Icons.Material.TwoTone.Casino;
+        public const string D20 = _baseClass + "dice-twenty-faces-twenty";
     }
 }
